c58bc65b4263f64ef0c54c9f5f0b99a93919eb22
Cristian <dcris3344@gmail.com>
2024-08-23 16:44:27 -0500

    Agregando titulo a los marcadores

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ fetch('lugares.json')
             let sitio = L.marker([data[i].corX,data[i].corY])
 
             sitio.addTo(map).bindPopup(`<div class="popup"><h1>${data[i].titulo}</h1><p>${data[i].descripcion}</p><div class="img__popup"><img src="${data[i].imageUrl}"></div></div>`)
+
+            sitio.addTo(map).bindTooltip(`<span>${data[i].titulo_corto}</span>`).openTooltip();
         
         }
     });
